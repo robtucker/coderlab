e199ed0437f08dfbd4e95200823bef687a95a191
robtucker <robtucker@users.noreply.github.com>
2016-12-14 16:41:03 +0000

    updates mocha test framework

---
+++ test/api/base-api.spec.js
@@ -0,0 +1,33 @@
+import * as mocha from "mocha";
+import { BaseApi } from "../../src/api/base-api";
+import * as Faker from "faker";
+
+let api = new BaseApi();
+api._entity = 'entity';
+
+describe('Base Api', function() {
+
+
+    describe('create action type', function() {
+
+        it('should create a type from a method entity and endpoint', function() {
+
+            let param = Faker.random.uuid();
+            let actionName = api._createActionType('METHOD', 'foo/:baz');
+
+            expect(actionName).to.equal('API_METHOD_ENTITY_FOO')
+        });
+
+        it('should not include variable parameters in the action', function() {
+
+            let param = Faker.random.uuid();
+            let actionName = api._createActionType('METHOD', ':dont/include/:random/:jibberish/words');
+
+            expect(actionName).to.equal('API_METHOD_ENTITY_INCLUDE_WORDS');
+
+        });
+
+
+
+    })
+})
---
+++ test/app.spec.js
@@ -0,0 +1,9 @@
+import 'babel-polyfill';
+import * as mocha from "mocha";
+
+describe('API', function() {
+
+    require('./api/base-api.spec.js');
+
+})
+
